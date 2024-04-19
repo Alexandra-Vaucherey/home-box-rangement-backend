6083eff164afe7aa2bcb1cbfd64040531f1d7893
Alexandra <shivaotaku@gmail.com>
2024-04-19 16:39:47 +0200

    Advanced signup users

--- models/users.js
+++ models/users.js
@@ -1,10 +1,30 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-  username: String,
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    message: ({value}) => `Username ${value} is not valid`
+  },
   phone: Number,
-  email: String,
-  password: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    select: false,
+    validate: {
+      validator: val => /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(val),
+      message: ({value}) => `${value} is not a valid email address.`,
+      }
+  },
+  password: {
+    type: String,
+    select: false,
+    required: () => this.status !== "pending",
+  },
   token: String,
   
  });
